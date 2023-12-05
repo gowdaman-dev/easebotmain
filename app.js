ce1d1ad53cf29cba07f38892011ea8e693910dda
Gowdaman p <56969439+gowdaman-dev@users.noreply.github.com>
2023-12-05 14:47:27 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+
+window.addEventListener('keypress', (ky) => {
+    Key = ky.key
+    if( Key == "N" || Key == 'n'){
+        console.log('REDIRECTING TO NAVIGATION SYSTEM..');
+        window.location.href = "https://jahangeer7704.github.io/navigation/"
+        
+    }
+    if( Key == "E" || Key == 'e'){
+        console.log('REDIRECTING TO EASEBOT..');
+        window.location.href = "https:easebot.netlify.app/"
+    }
+})
