da8e90fb23014755cc51b1b55ed59d87c28573a9
Gowdaman p <56969439+gowdaman-dev@users.noreply.github.com>
2023-12-05 14:56:42 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ window.addEventListener('keypress', (ky) => {
     Key = ky.key
     if( Key == "N" || Key == 'n'){
         console.log('REDIRECTING TO NAVIGATION SYSTEM..');
-        window.location.href = "https://jahangeer7704.github.io/navigation/"
+        window.location.replace = "https://jahangeer7704.github.io/navigation/"
         
     }
     if( Key == "E" || Key == 'e'){
         console.log('REDIRECTING TO EASEBOT..');
-        window.location.href = "https://easebot.netlify.app/"
+        window.location.replace = "https://easebot.netlify.app/"
     }
 })
